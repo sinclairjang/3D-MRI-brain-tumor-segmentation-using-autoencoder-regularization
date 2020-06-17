2bf57ea2d01523a24487269cbb3778d454afdbb3
Young <sinclairjang@gmail.com>
2020-06-17 18:02:05 +0900

    module commit

---
+++ run_notebook.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import nbformat, fire
+from nbconvert.preprocessors import ExecutePreprocessor
+
+def run_notebook(path):
+    nb = nbformat.read(open(path), as_version=nbformat.NO_CONVERT)
+    ExecutePreprocessor(timeout=600).preprocessor(nb, {})
+    
+if __name__ == '__main__': fire.Fire(run_notebook)
